0f1024255b5e39ac9bf4c396dded90cdc9835299
Fabiola <fabiola.aurelien@gmail.com>
2026-07-19 13:09:20 -0400

    Rendered potential home screen

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,14 +1,8 @@
-import { useState } from 'react'
 import './App.css'
+import Home from './pages/Home/Home'
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-     
-    </>
-  )
+  return <Home />
 }
 
 export default App
