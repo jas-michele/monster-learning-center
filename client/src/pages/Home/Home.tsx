b62d5c2d6c81734389b78654d862a52c7973d5f0
Fabiola <fabiola.aurelien@gmail.com>
2026-07-21 21:01:45 -0400

    Build initial Home page and avatar assets

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,86 +1,57 @@
 import React, { useState } from 'react'
 import './Home.css'
 import bg from '../../assets/playhouseBG.png'
-import Avatar1 from '../../assets/avatar1.png'
-import Avatar2 from '../../assets/avatar2.png'
-import Avatar3 from '../../assets/avatar3.png'
-import Avatar4 from '../../assets/avatar4.png'
-import Avatar5 from '../../assets/avatar5.png'
-import mechanicNav from '../../assets/mechanicNav.png'
-import storyTimeNav from '../../assets/storyTimeNav.png'
-import racetrackNav from '../../assets/racetrackNav.png'
-import dinohuntNav from '../../assets/dinohuntNav.png'
+import Avatar, { type AvatarOutfit } from '../../components/Avatar'
 
 const Home: React.FC = () => {
-  const [avatarSrc, setAvatarSrc] = useState(Avatar1)
-  const isLandscapeAvatar = avatarSrc === Avatar5
+  const [outfit, setOutfit] = useState<AvatarOutfit>('casual')
+  const isStorytime = outfit === 'storytime'
 
-  const hoverAvatar = (avatar: string) => () => setAvatarSrc(avatar)
-  const resetAvatar = () => setAvatarSrc(Avatar1)
+  const hoverAvatar = (avatarOutfit: AvatarOutfit) => () => setOutfit(avatarOutfit)
+  const resetAvatar = () => setOutfit('casual')
 
   return (
     <div className="home" role="main" aria-label="Playhouse home screen">
-      <div className="home__bg" style={{ backgroundImage: `url(${bg})` }} aria-hidden />
+      <div className="home__scene">
+        <div className="home__bg" style={{ backgroundImage: `url(${bg})` }} aria-hidden />
 
-      <div className="home__inner">
-        <div className="home__stage">
-          <div className="home__nav-layer">
-            <div className="home__nav-groups">
-              <div className="home__nav-group home__nav-group--left">
-                <button
-                  className="home__nav-button"
-                  aria-label="Mechanic shop"
-                  onMouseEnter={hoverAvatar(Avatar2)}
-                  onMouseLeave={resetAvatar}
-                  onFocus={hoverAvatar(Avatar2)}
-                  onBlur={resetAvatar}
-                >
-                  <img src={mechanicNav} alt="Mechanic shop" className="home__nav-icon" />
-                </button>
-                <button
-                  className="home__nav-button"
-                  aria-label="Race track"
-                  onMouseEnter={hoverAvatar(Avatar3)}
-                  onMouseLeave={resetAvatar}
-                  onFocus={hoverAvatar(Avatar3)}
-                  onBlur={resetAvatar}
-                >
-                  <img src={racetrackNav} alt="Race track" className="home__nav-icon" />
-                </button>
-              </div>
+        <nav className="home__nav-hotspots" aria-label="Play areas">
+          <button
+            className="home__nav-hotspot home__nav-hotspot--storytime"
+            aria-label="Story time"
+            onMouseEnter={hoverAvatar('storytime')}
+            onMouseLeave={resetAvatar}
+            onFocus={hoverAvatar('storytime')}
+            onBlur={resetAvatar}
+          />
+          <button
+            className="home__nav-hotspot home__nav-hotspot--mechanic"
+            aria-label="Mechanic shop"
+            onMouseEnter={hoverAvatar('mechanic')}
+            onMouseLeave={resetAvatar}
+            onFocus={hoverAvatar('mechanic')}
+            onBlur={resetAvatar}
+          />
+          <button
+            className="home__nav-hotspot home__nav-hotspot--racetrack"
+            aria-label="Race track"
+            onMouseEnter={hoverAvatar('race')}
+            onMouseLeave={resetAvatar}
+            onFocus={hoverAvatar('race')}
+            onBlur={resetAvatar}
+          />
+          <button
+            className="home__nav-hotspot home__nav-hotspot--dino"
+            aria-label="Dino hunt"
+            onMouseEnter={hoverAvatar('safari')}
+            onMouseLeave={resetAvatar}
+            onFocus={hoverAvatar('safari')}
+            onBlur={resetAvatar}
+          />
+        </nav>
 
-              <div className="home__nav-group home__nav-group--right">
-                <button
-                  className="home__nav-button"
-                  aria-label="Dino hunt"
-                  onMouseEnter={hoverAvatar(Avatar4)}
-                  onMouseLeave={resetAvatar}
-                  onFocus={hoverAvatar(Avatar4)}
-                  onBlur={resetAvatar}
-                >
-                  <img src={dinohuntNav} alt="Dino hunt" className="home__nav-icon" />
-                </button>
-                <button
-                  className="home__nav-button"
-                  aria-label="Story time"
-                  onMouseEnter={hoverAvatar(Avatar5)}
-                  onMouseLeave={resetAvatar}
-                  onFocus={hoverAvatar(Avatar5)}
-                  onBlur={resetAvatar}
-                >
-                  <img src={storyTimeNav} alt="Story time" className="home__nav-icon" />
-                </button>
-              </div>
-            </div>
-          </div>
-
-          <div className="home__avatar-wrap" aria-hidden>
-            <img
-              src={avatarSrc}
-              alt="Avatar standing in the center of the playhouse rug"
-              className={`home__avatar${isLandscapeAvatar ? ' home__avatar--landscape' : ''}`}
-            />
-          </div>
+        <div className={`home__avatar-wrap${isStorytime ? ' home__avatar-wrap--storytime' : ''}`} aria-hidden>
+          <Avatar outfit={outfit} animation="idle" className="home__avatar" />
         </div>
       </div>
     </div>
